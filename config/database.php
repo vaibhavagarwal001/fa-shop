2819f97fdd6a61240f159de8425ca840e0259b14
floweraura <vaibhav.agarwal@bakingo.com>
2019-12-06 12:33:10 +0530

    FA DB added

--- config/database.php
+++ config/database.php
@@ -109,6 +109,25 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'floweraura_mysql' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('FLOWERAURA_DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('FLOWERAURA_DB_NAME', 'bakingo'),
+            'username' => env('FLOWERAURA_DB_USERNAME', 'root'),
+            'password' => env('FLOWERAURA_DB_PASSWORD', 'root'),
+            'unix_socket' => env('DB_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
     ],
 
